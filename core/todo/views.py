dea451e89824a9412e0ac501a0daba45ccca7520
iliya-askari <elyaaskarii86@gmail.com>
2024-05-31 13:43:57 +0430

    create cbv and set url in base

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView , UpdateView, DeleteView,CreateView , View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
 # Create your views here.
+class TaskList(ListView):
+    template_name = 'index.html'
+    model = Task
+class TaskCreate(CreateView):
+    template_name = 'index.html'
+    model = Task
+class TaskUpdate(UpdateView):
+    template_name = 'index.html'
+    model = Task
+
+class TaskComplete(View):
+    template_name = 'index.html'
+    model = Task
+class TaskDeleteView(DeleteView):
+    template_name = 'index.html'
+    model = Task
