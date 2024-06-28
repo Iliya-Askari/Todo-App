9a89123c488d3305d88f9c8d19cfdf031d2da06c
iliya-askari <elyaaskarii86@gmail.com>
2024-06-29 01:41:02 +0430

    complate create token

--- core/accounts/api/v1/serializer.py
+++ core/accounts/api/v1/serializer.py
@@ -25,3 +25,23 @@ class RegestrationsSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password_2', None)
         return User.objects.create_user(**validated_data)
+    
+class CustomLoginTokenSerializer(serializers.Serializer):
+    username = serializers.CharField(label=_("Username"),write_only=True)
+    password = serializers.CharField(label=_("Password"),style={'input_type': 'password'},trim_whitespace=False,write_only=True)
+    token = serializers.CharField(label=_("Token"),read_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        request = self.context.get('request')
+        if username and password :
+            user = authenticate(request=request,username=username, password=password)
+            if not user:
+                raise serializers.ValidationError(_('Invalid credentials'))
+        else :
+            raise serializers.ValidationError(_('Username and password required'))
+
+        attrs['user'] = user
+        return attrs
+    
--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import view
 
 urlpatterns =[
-    path('regstrations/',view.RegestrationsApiView.as_view(),name='regstrations')
+    path('regstrations/',view.RegestrationsApiView.as_view(),name='regstrations'),
+    path('token/create/',view.CustomLoginTokenApiView.as_view(),name='crate-token')
 ]
