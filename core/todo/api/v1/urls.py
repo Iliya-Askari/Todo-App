381da8e04a5f97373abb37dd67d05c80a043d4ff
iliya-askari <elyaaskarii86@gmail.com>
2024-07-06 17:33:26 +0430

    update

--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -4,6 +4,9 @@ from .views import *
 """
 Create api  URL with the router
 """
+
+app_name = "api-v1"
+
 router = routers.DefaultRouter()
 router.register("Tasklist", TaskViewSet, basename="tasklist")
 
--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from todo.models import Task
 from .serializer import TaskSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly , IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from .pagination import LargeResultsSetPagination
@@ -13,7 +13,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     In this class, permissions and filters are implemented to improve performance
     """
 
-    permission_classes = [IsAuthenticatedOrReadOnly, IsUserOrReadOnly]
+    permission_classes = [IsAuthenticated, IsUserOrReadOnly]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
