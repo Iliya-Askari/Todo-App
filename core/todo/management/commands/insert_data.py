bfe3d07086c79a5727d72ab5fed223fc1983bf6a
iliya-askari <elyaaskarii86@gmail.com>
2024-07-10 16:59:11 +0430

    change create task

--- core/todo/management/commands/insert_data.py
+++ core/todo/management/commands/insert_data.py
@@ -1,8 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from datetime import datetime
 import random
-from accounts.models import Users , Profile
+from accounts.models import Users
 from todo.models import Task
 
 
@@ -24,19 +23,14 @@ class Command(BaseCommand):
         self.fake = Faker()
 
     def handle(self, *args, **options):
-        profiles = []
-        for _ in range(10):
+        users = []
+        for _ in range(5):
             user = Users.objects.create_user(email=self.fake.email(), password='Test@123456')
-            profile = Profile.objects.get(user=user)
-            profile.first_name = self.fake.first_name()
-            profile.last_name = self.fake.last_name()
-            profile.description = self.fake.paragraph(nb_sentences=3)
-            profile.save()
-            profiles.append(profile)
+            users.append(user)
         
-        for _ in range(10):
+        for _ in range(5):
             Task.objects.create(
-                user = random.choice(profiles),
+                user = random.choice(users),
                 title = self.fake.paragraph(nb_sentences=1),
                 complete = self.fake.boolean(),
             )
