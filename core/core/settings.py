a62dc9797c34ab4da21796e643e5c7000f3b8583
iliya-askari <elyaaskarii86@gmail.com>
2024-07-20 01:27:19 +0430

    setup Environment prod database postgers

--- core/core/settings.py
+++ core/core/settings.py
@@ -87,16 +87,40 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8081",
+    "http://localhost:8081",
+]
+
+if DEBUG:
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+else:
+    CSRF_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = False
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', 'mydatabase'),
+        'USER': config('DB_USER', 'myuser'),
+        'PASSWORD': config('DB_PASSWORD', 'mypassword'),
+        'HOST': config('DB_HOST', 'db'),
+        'PORT': config('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
